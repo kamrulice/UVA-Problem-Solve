c83d9dfa690967479ab88295e8656ff2b7bb6c87
kamrulice <mdkamrulhasan989@gmail.com>
2016-08-06 23:58:02 +0600

    Update and rename uva 10071.c to UVA 10071 (Back to High School Physics).c

--- UVA 10071 (Back to High School Physics).c
+++ UVA 10071 (Back to High School Physics).c
@@ -1,8 +1,12 @@
+/*
+    Email:mdkamrulhasan989@gmail.com
+*/
+
 #include <stdio.h>
 
 int main(){
     int v,t,k;
-    while(scanf("%d %d",&v,&t)!=EOF){
+    while(scanf("%d %d",&v,&t)!=EOF){ // s = 2vt
         k=2*v*t;
         printf("%d\n",k);
     }
