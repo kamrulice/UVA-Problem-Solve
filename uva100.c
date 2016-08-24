5ff0c28f8cc70e66d8d3bdcafd65818686ca918a
Kamrul Hasan <mdkamrulhasan989@gmail.com>
2016-08-25 03:07:11 +0600

    Add files via upload

---
+++ uva100.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    long long int p,m,i,j,k,swap,count;
+    while(scanf("%lld %lld",&i,&j)!=EOF){
+            printf("%lld %lld ",i,j);
+        p=0;
+        if(i>j){
+            swap=i;
+            i=j;
+            j=swap;
+        }
+        for(k=i;k<=j;k++){
+           m=k;
+            count=1;
+            while(m!=1){
+                if(m%2!=0){
+                    m=3*m+1;
+                    }
+                else{
+                    m=m/2;
+                }
+                if(m==1){
+                    count++;
+                    break;
+                }
+                else ++count;
+            }
+            if(p<count){
+                p=count;
+            }
+            }
+            printf("%lld\n",p);
+    }
+    return 0;
+}
